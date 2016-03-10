b82d7c9b3c67ba8e8f1d80b19a2bac8099053595
Miguel Román <miguelerm@gmail.com>
2016-03-10 08:14:18 -0600

    Logica de reintentar en la configuración de autenticación de la REST API.

--- src/CSharpRestBackend/Startup.cs
+++ src/CSharpRestBackend/Startup.cs
@@ -1,9 +1,13 @@
-﻿
-
-using IdentityServer3.AccessTokenValidation;
+﻿using IdentityServer3.AccessTokenValidation;
 using Microsoft.Owin.Cors;
 using Newtonsoft.Json.Serialization;
 using Owin;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
 using System.Web.Http;
 
 namespace CSharpRestBackend
@@ -15,17 +19,7 @@ namespace CSharpRestBackend
             // Allow all origins
             app.UseCors(CorsOptions.AllowAll);
 
-            // Wire token validation
-            app.UseIdentityServerBearerTokenAuthentication(new IdentityServerBearerTokenAuthenticationOptions
-            {
-                Authority = "http://localhost:15001",
-
-                // For access to the introspection endpoint
-                ClientId = "api",
-                ClientSecret = "api-secret",
-
-                RequiredScopes = new[] { "api" }
-            });
+            ConfigurarAutenticacion(app);
 
             // Wire Web API
             var httpConfiguration = new HttpConfiguration();
@@ -39,6 +33,53 @@ namespace CSharpRestBackend
             serializerSettings.DateTimeZoneHandling = Newtonsoft.Json.DateTimeZoneHandling.Utc;
 
             app.UseWebApi(httpConfiguration);
+        }
+
+        private static void ConfigurarAutenticacion(IAppBuilder app) {
+
+            try
+            {
+                Trace.TraceInformation("Intentando configurar el mecanismos de autenticación por tokens...");
+
+                // Wire token validation
+                app.UseIdentityServerBearerTokenAuthentication(new IdentityServerBearerTokenAuthenticationOptions
+                {
+                    Authority = "http://localhost:15001",
+
+                    // For access to the introspection endpoint
+                    ClientId = "api",
+                    ClientSecret = "api-secret",
+
+                    RequiredScopes = new[] { "api" }
+                });
+
+                Trace.TraceInformation("Mecanismo de autenticación configurado.");
+            }
+            catch (InvalidOperationException ex)
+            {
+                var innerException = ex.InnerException as IOException;
+                if (innerException == null)
+                {
+                    throw;
+                }
+
+                var innerExceptions = innerException.InnerException as AggregateException;
+
+                if (innerExceptions == null)
+                {
+                    throw;
+                }
+                
+                if (!innerExceptions.InnerExceptions.Any(x => x is HttpRequestException))
+                {
+                    throw;
+                }
+
+                Trace.TraceError("No se pudo conectar con el AuthorityServer, intentando de nuevo en un segundo...");
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                ConfigurarAutenticacion(app);
+            }
+           
         }
     }
 }
